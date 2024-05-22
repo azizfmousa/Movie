cc89e7eb96f3e80b32f784667128a0f365173249
abdelaziz <aziz.mousa@yahoo.com>
2024-05-22 13:49:26 +0300

    change api from http to dio

--- lib/data/movie.dart
+++ lib/data/movie.dart
@@ -1,13 +1,12 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 class Movie {
   String title;
   String image;
-  num popularity;
+  // num popularity;
   num? voteAvarage;
   Movie({
     required this.title,
     required this.image,
-    required this.popularity,
+    // required this.popularity,
     required this.voteAvarage,
   });
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-import 'package:http/http.dart' as http;
 import 'package:movie_app/data/movie.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,61 +13,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Movie> movies = [];
+
   Future<List<Movie>> getData() async {
-    final url = Uri.parse('https://api.themoviedb.org/3/person/popular');
+    final dio = Dio();
 
-    final response = await http.get(url, headers: {
-      'Authorization':
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGU3Zjg2ZDUxZmQ5ZjFjMTFkNDFiZWE0OWUxNGYyNSIsInN1YiI6IjU3OWY1OTRjYzNhMzY4MTZlYjAwMWFiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6fBb-vZSqVehkMduXoIE9O0BZKEZTnBPtA9GJ7gwiTc'
-    });
+    const keyApi =
+        'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGU3Zjg2ZDUxZmQ5ZjFjMTFkNDFiZWE0OWUxNGYyNSIsInN1YiI6IjU3OWY1OTRjYzNhMzY4MTZlYjAwMWFiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6fBb-vZSqVehkMduXoIE9O0BZKEZTnBPtA9GJ7gwiTc';
 
-    final responseData = json.decode(response.body);
+    final response = await dio.get('https://api.themoviedb.org/3/movie/popular',
+        options: Options(headers: {"Authorization": "Bearer $keyApi"}));
+    // final url = Uri.parse();
 
-    List data = responseData['results'];
+    // final response = await http.get(url, headers: {
+    //   'Authorization':
+    //       'Bearer '
+    // });
 
-    List<Movie> movies = [];
+    // final responseData = json.decode(response.body);
 
-    for (dynamic movie in data) {
+    List data = response.data['results'];
+
+    for (final movie in data) {
       Movie move = Movie(
-        title: movie['original_name'],
+        title: movie['title'],
         image: 'http://image.tmdb.org/t/p/w185/${movie['poster_path']}',
-        popularity: movie['popularity'],
+        // popularity: movie['popularity'],
         voteAvarage: movie['vote_average'],
       );
       movies.add(move);
+      log(movie.toString());
     }
-
-    setState(() {});
-    log(movies.toString());
 
     return movies;
   }
 
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getData();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Movies Page'),
-      ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Movies Page'),
         ),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Image.network(
-            'https://static.vecteezy.com/system/resources/thumbnails/036/324/708/small/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg',
-            fit: BoxFit.cover,
-          );
-        },
-      ),
-    );
+        body: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            log(snapshot.connectionState.toString());
+            log(snapshot.hasData.toString());
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return GridView.builder(
+                  itemCount: snapshot.data!.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return Image.network(
+                      snapshot.data![index].image,
+                      fit: BoxFit.cover,
+                    );
+                  },
+                );
+              } else {
+                return const Text("error");
+              }
+            }
+            return const Text('data');
+          },
+        ));
   }
 }
